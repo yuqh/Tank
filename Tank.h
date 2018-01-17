f774debc58b0848f6c36ef06b661c8cad29430c3
yuqh <1259670234@qq.com>
2018-01-17 14:30:21 +0800

    Tank Battle Game

---
+++ Tank.h
@@ -0,0 +1,13 @@
+
+#if !defined(AFX_TANK_H__1B93040C_698E_4202_9B64_F3B60F99F91A__INCLUDED_)
+#define AFX_TANK_H__1B93040C_698E_4202_9B64_F3B60F99F91A__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "resource.h"
+
+// DownLoad By http://www.veryhuo.com
+
+#endif // !defined(AFX_TANK_H__1B93040C_698E_4202_9B64_F3B60F99F91A__INCLUDED_)
